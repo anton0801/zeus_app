1026d9322fd67430d62815c16864a5f1e6e1f0e6
Anton <danilovanton724@gmail.com>
2024-04-30 15:06:38 +0200

    Initial Commit

---
+++ ZeusApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZeusApp
+//
+//  Created by Anton on 30/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZeusApp/ZeusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeusAppApp.swift
+//  ZeusApp
+//
+//  Created by Anton on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ZeusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
